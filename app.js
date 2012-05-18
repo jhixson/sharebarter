eaf0c30e08f9a232421e1d0f53adaf3715e30059
Jesse Hixson <hixsonj@gmail.com>
2012-05-17 23:07:50 -0400

    Added PORT environment variable.

--- app.js
+++ app.js
@@ -36,5 +36,5 @@ app.get('/signup', routes.signup);
 app.post('/register', routes.register);
 app.post('/finish', routes.finish);
 
-app.listen(3000);
+app.listen(process.env.PORT || 3000);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
