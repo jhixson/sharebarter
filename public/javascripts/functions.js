41c0704204332834c5b96995abb81e939913b13a
Jesse Hixson <hixsonj@gmail.com>
2012-05-17 22:25:19 -0400

    Added custom select box plugin. Prepping for heroku deploy.

--- public/javascripts/functions.js
+++ public/javascripts/functions.js
@@ -2,5 +2,7 @@ $(document).ready(function() {
   $('#sign_up_button, #done_button').click(function(e) {
     e.preventDefault();
     $(this).closest('form').submit();
-  })
+  });
+
+  $('.select').customSelect();
 });
